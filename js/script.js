336ac338fd9287e64c2e2cd33aa1119655b56166
AlessandroVola <volaalessandro@gmail.com>
2022-08-29 09:58:21 +0200

    rectify dice's math.random

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // genera un numero da 1 a 6 randomico per te e per il computer, chi fa il numero più alto vince
 
-var userDice = Math.floor(Math.random() * 7)
-var pcDice = Math.floor(Math.random() * 7)
+var userDice = Math.floor(Math.random() * 6) + 1
+var pcDice = Math.floor(Math.random() * 6) + 1
 
 console.log("Punteggio Utente = " + userDice)
 console.log("Punteggio Pc = " + pcDice)
@@ -14,6 +14,7 @@ if (userDice > pcDice) {
 else {
     console.log("Pc vince")
 }
+
 
 
 
